947da0976114e239ad0475294b63a3516e22895e
Michael Jones <jonesmic@MacBook-Pro.localdomain>
2023-12-15 16:23:31 -0500

    REverb works but its noisy

--- projects/SimpleTouchReverb/SimpleTouchReverb.cpp
+++ projects/SimpleTouchReverb/SimpleTouchReverb.cpp
@@ -14,38 +14,33 @@ void AudioCallback(AudioHandle::InputBuffer in,
 {
 	float dry, send, wetl, wetr; // Effects Vars
 
-	for (size_t i = 0; i < size; i++)
-	{
-		// Create Reverb Send
+       for (size_t i = 0; i < size; i++)
+       {
+               // Create Reverb Send
         dry  = in[0][i];
-        send = dry * 0.6f;
+        send = dry * 0.8f;
         verb.Process(send, send, &wetl, &wetr);
 
 		// Output
-		//out[0][i] = dry + wetl;
-		//out[1][i] = dry + wetr;
-
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+		out[0][i] = dry + wetl;
+		out[1][i] = dry + wetr;
+       }
 }
 
 int main(void)
 {
-
-	
-	hw.Init();
-	
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
-	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
-	hw.StartAudio(AudioCallback);
-
+    /** Initialize the hardware */
 	float samplerate;
-    samplerate = hw.AudioSampleRate();
-	verb.Init(samplerate);
-    verb.SetFeedback(0.85f);
-    verb.SetLpFreq(2000.0f);
+    hw.Init();
 
+    /** Start Processing the audio */
+    hw.StartAudio(AudioCallback);
+
+	samplerate = hw.AudioSampleRate();
+    verb.Init(samplerate);
+    verb.SetFeedback(0.99f);
+    verb.SetLpFreq(2000.0f);
+	
 	hw.SetLed(true);
 
 

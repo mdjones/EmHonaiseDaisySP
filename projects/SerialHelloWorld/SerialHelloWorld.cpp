11a41800430d21ba4cf4299e5810755c936b62e6
Michael Jones <mdjones71@gmail.com>
2023-12-24 16:25:32 -0500

    Trying to read a knob

--- projects/SerialHelloWorld/SerialHelloWorld.cpp
+++ projects/SerialHelloWorld/SerialHelloWorld.cpp
@@ -2,23 +2,41 @@
 #include "daisysp.h"
 
 using namespace daisy;
+using namespace daisysp;
 
 DaisySeed hw;
-daisysp::Oscillator osc;
+Oscillator sineOsc;
+Oscillator triOsc;
+
 
 #define S10 daisy::seed::D9 // SWITCH : ORDERED / AS PLAYED
+#define S31 daisy::seed::A1.pin // FADER : ORDERED / AS PLAYED
+
+// Create a GPIO object
+GPIO my_switch;
+GPIO knob1;
+
 
 void AudioCallback(AudioHandle::InputBuffer in, 
                 AudioHandle::OutputBuffer out, 
                 size_t size) 
 {
+
+	
     for (size_t i = 0; i < size; i++)
     {
         // The oscillator's Process function synthesizes, and
         // returns the next sample.
-        float sine_signal = osc.Process();
-        out[0][i] = sine_signal;
-        out[1][i] = sine_signal;
+        float sine_signal = sineOsc.Process();
+		float tri_signal = triOsc.Process();
+
+		if (my_switch.Read()){
+			out[0][i] = sine_signal * tri_signal;
+    		out[1][i] = sine_signal * tri_signal;
+		}else{
+        	out[0][i] = sine_signal;
+        	out[1][i] = sine_signal;
+		}
     }
 }
 
@@ -29,14 +47,23 @@ int main(void) {
 
     // We initialize the oscillator with the sample rate of the hardware
     // this ensures that the frequency of the Oscillator will be accurate.
-    osc.Init(hw.AudioSampleRate());
-    hw.StartAudio(AudioCallback);
+    sineOsc.Init(hw.AudioSampleRate());
+	sineOsc.SetWaveform(Oscillator::WAVE_SIN);
+	triOsc.Init(hw.AudioSampleRate());
+	triOsc.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI);
 
-  // Create a GPIO object
-  GPIO my_switch;
+    hw.StartAudio(AudioCallback);
 
   // Initialize the GPIO object
   my_switch.Init(S10, GPIO::Mode::INPUT, GPIO::Pull::NOPULL);
+
+  AdcChannelConfig adcConfig;
+  adcConfig.InitSingle(hw.GetPin(S31));
+
+  //Initialize the adc with the config we just made
+  hw.adc.Init(&adcConfig, 1); 
+  //Start reading values
+  hw.adc.Start();
 
   // Enable Logging, and set up the USB connection.
   hw.StartLog(true);
@@ -50,9 +77,11 @@ int main(void) {
     // "button_state"
     bool switch_state = my_switch.Read();
     hw.SetLed(switch_state);
+	
 	if (init_switch_state != switch_state){
 		hw.PrintLine("Switch S10 (D9) state: %d\n", static_cast<int>(switch_state));
 		init_switch_state = switch_state;
+		hw.PrintLine("Fader1 state: [%.3f]\n", hw.adc.GetFloat(0));
 	}
   }
 }

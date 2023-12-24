813b5594ee3d722fbb1bea7a22abf741be50ab00
Michael Jones <mdjones71@gmail.com>
2023-12-24 13:35:34 -0500

    Print a readout of switch state

--- projects/SerialHelloWorld/SerialHelloWorld.cpp
+++ projects/SerialHelloWorld/SerialHelloWorld.cpp
@@ -23,10 +23,15 @@ int main(void) {
   // And Print Hello World!
   hw.PrintLine("Hello World. You all bitches!!!!!!!");
 
+  bool init_switch_state = my_switch.Read();
   while (1) {
     // And let's store the state of the button in a variable called
     // "button_state"
     bool switch_state = my_switch.Read();
     hw.SetLed(switch_state);
+	if (init_switch_state != switch_state){
+		hw.PrintLine("Switch S10 state: %d\n", static_cast<int>(switch_state));
+		init_switch_state = switch_state;
+	}
   }
 }

daef2bdab5e191ef9a0f69367bb3f63b5f142556
Michael Jones <mdjones71@gmail.com>
2023-12-24 13:21:14 -0500

    Connect swith to hw LED

--- projects/SerialHelloWorld/SerialHelloWorld.cpp
+++ projects/SerialHelloWorld/SerialHelloWorld.cpp
@@ -3,11 +3,20 @@
 using namespace daisy;
  
 DaisySeed hw;
+
+#define S10 daisy::seed::D9 // SWITCH : ORDERED / AS PLAYED
  
 int main(void) {
  
   // Initialize the Daisy Seed Hardware
   hw.Init();
+
+  // Create a GPIO object
+  GPIO my_switch;
+ 
+  // Initialize the GPIO object
+  my_switch.Init(S10, GPIO::Mode::INPUT, GPIO::Pull::PULLUP);
+ 
  
   // Enable Logging, and set up the USB connection.
   hw.StartLog(true);
@@ -15,5 +24,9 @@ int main(void) {
   // And Print Hello World!
   hw.PrintLine("Hello World. You all bitches!!!!!!!");
  
-  while(1) {}
+  while(1) {    
+		// And let's store the state of the button in a variable called "button_state"
+		bool switch_state = my_switch.Read();
+		hw.SetLed(switch_state);
+	}
 }
